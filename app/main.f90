9c1ab2cae21ef87cdd1e56fcb82a95d8aa1921a0
Vlad Busev <buvlad2002@gmail.com>
2026-08-13 14:00:23 +0300

    :hammer: Add fpm packet
    Add initial project files and main program structure

---
+++ app/main.f90
@@ -0,0 +1,5 @@
+program main
+    implicit none
+
+    print *, "hello from project iceberg-thermodynamic-model"
+end program main
---
+++ src/iceberg-thermodynamic-model.f90
@@ -0,0 +1,10 @@
+module iceberg_thermodynamic_model
+    implicit none
+    private
+
+    public :: say_hello
+contains
+    subroutine say_hello
+        print *, "Hello, iceberg-thermodynamic-model!"
+    end subroutine say_hello
+end module iceberg_thermodynamic_model
